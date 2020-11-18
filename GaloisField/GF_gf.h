0ef43927b20afc4c9dfaf285ccb8b99901d7eaa7
Kei N <keink@users.noreply.github.com>
2020-11-18 09:04:34 +0900

    Added codes for GaloisField

---
+++ GaloisField/GF_gf.h
@@ -0,0 +1,202 @@
+#pragma once
+#include <iostream>
+#include <math.h>
+#include <stdlib.h>
+#include <numeric>
+#include "Field_gf.h"
+#include <boost/multiprecision/cpp_int.hpp>
+
+typedef long long ll;
+
+extern ll prime_number;
+/*extern std::vector< std::vector<ll> > product_table;
+extern std::vector< std::vector<ll> > sum_table;
+extern std::vector<ll> inverse_table;
+*/
+
+ll mod(ll x, ll p){
+    if(x>=0){
+        return x%p;
+    }
+    else{
+        return (x+((-x)/p+1)*p)%p;
+    }
+}
+
+ll extentedEuglid(ll a,ll b, ll& x, ll& y)
+{
+    if(b!=0){
+        ll d=extentedEuglid(b,a%b,y,x);
+        y-=(a/b)*x;
+        return d;
+    }
+    
+        x=1;y=0;
+
+    return a;
+}
+
+class GF: public Field
+{
+    
+public:
+ll num;
+    GF(ll n)
+    {
+        num=mod(n,prime_number);
+    }
+
+    GF()
+    {
+        num=0;
+    }
+
+    ~GF()
+    {
+
+    }
+
+    GF operator+()const{return *this;}
+    GF operator-()const{return GF(prime_number-num);}
+
+    ///////
+    GF inverse()const{
+        ll x; ll y;
+        ll d=extentedEuglid(num,prime_number,x,y);
+        return GF(x);
+    }
+
+    bool isZero()
+    {
+        return mod(num,prime_number)==0;
+    }
+
+    bool operator==(const GF &x)
+    {
+        return mod(num,prime_number)==mod(x.num,prime_number);
+    }
+
+    bool operator!=(const GF & x){
+        return !(*this==x);
+    }
+
+    bool operator<(const GF& x){
+        return (num<x.num);
+    }
+
+    GF& operator=(const GF& x)
+    {
+        num=x.num;
+        return *this;
+    }
+
+    GF operator+(const GF& x)
+    {
+        return GF(num+x.num);
+    }
+
+    GF &operator+=(const GF& x){
+        *this=*this+x;
+    }
+
+    GF operator-(const GF& x){
+        return *this+(-x);
+    }
+
+    GF &operator-=(const GF& x)
+    {
+        *this=*this-x;
+    }
+
+    GF operator*(const GF& x)
+    {
+        return GF(num*x.num);
+    }
+
+    GF &operator*=(const GF& x)
+    {
+        *this=*this*x;
+    }
+
+    //////////
+    GF operator/(const GF& x)
+    {
+        return (*this)*x.inverse();
+    }
+
+    GF &operator/=(const GF& x){
+        *this=*this/x;
+    }
+
+    void output()
+    {
+        std::cout<<num;
+    }
+
+    void input()
+    {
+        std::cin>>num;
+        num=mod(num,prime_number);
+    }
+
+    void setZero()
+    {
+        num=0;
+    }
+
+    void setOne()
+    {
+        num=1;
+    }
+
+    bool operator<(const GF &x) const
+    {
+        return num<x.num;
+    }
+};
+
+//n番目までの素数を数える
+std::vector<ll> eratosthenes(ll n){
+    std::vector<bool> is_prime;
+    std::vector<ll> prime;
+
+    ll N=n*n;
+    is_prime.resize(N+1,true);
+
+    is_prime[0]=is_prime[1]=false;
+
+    for(ll i=2;i<=N;i++){
+        if(is_prime[i]==true){
+            prime.push_back(i);
+            for(ll j=2*i;j<=N;j+=i){
+                is_prime[j]=false;
+            }
+        }
+        if(prime.size()==n){
+            break;
+        }
+    }
+    return prime;
+
+}
+
+//n以下の素数を数える
+std::vector<ll> eratosthenes_(ll n){
+    std::vector<bool> is_prime;
+    std::vector<ll> prime;
+
+    is_prime.resize(n+1,true);
+
+    is_prime[0]=is_prime[1]=false;
+
+    for(ll i=2;i<=n;i++){
+        if(is_prime[i]==true){
+            prime.push_back(i);
+            for(ll j=2*i;j<=n;j+=i){
+                is_prime[j]=false;
+            }
+        }
+    }
+    return prime;
+
+}
---
+++ GaloisField/Matrix2_gf.h
@@ -0,0 +1,408 @@
+#pragma once
+#include <iostream>
+#include <vector>
+#include "Field_gf.h"
+#include "RationalNumber_gf.h"
+#include "GF_gf.h"
+#define Field2 GF
+//#include "Matrix_gf.h"
+
+class Matrix2 : public Field2
+{
+  public:
+	ll row;
+	ll col;
+	std::vector<std::vector<Field2>> X;
+
+	Matrix2(ll r, ll c)
+	{
+		row = r;
+		col = c;
+		X.resize(row);
+		for (ll i = 0; i < row; i++)
+		{
+			X[i].resize(col);
+		}
+
+		for (ll i = 0; i < row; i++)
+		{
+			for (ll j = 0; j < col; j++)
+			{
+				if (i == j)
+					X[i][j].setOne();
+				else
+					X[i][j].setZero();
+				//std::cout<<i<<"  "<<j<<std::endl;
+			}
+		}
+	}
+
+	Matrix2()
+	{
+		row = 0;
+		col = 0;
+	}
+
+	~Matrix2()
+	{
+		//std::cout<<row<<" "<<col<<std::endl;
+		//std::cout<<"Matrix デストラクタ"<<std::endl;
+		for (ll i = 0; i < row; i++)
+		{
+			X[i].clear();
+			X[i].shrink_to_fit();
+		}
+		X.clear();
+		X.shrink_to_fit();
+	}
+
+	Matrix2 operator -() const{
+		Matrix2 A(row,col);
+		for(ll i=0;i<row;i++){
+			for(ll j=0;j<col;j++){
+				A.X[i][j]=-X[i][j];
+			}
+		}
+		return A;
+	}
+
+	void input_matrix()
+	{
+		std::cin >> row >> col;
+
+		X.resize(row);
+		for (ll i = 0; i < row; i++)
+		{
+			X[i].resize(col);
+		}
+
+		for (ll i = 0; i < row; i++)
+		{
+			for (ll j = 0; j < col; j++)
+			{
+				X[i][j].input();
+				//std::cout<<i<<"  "<<j<<std::endl;
+			}
+		}
+	}
+
+	/*
+	void input_matrix_int()
+	{
+		std::cin>>row>>col;
+		X.resize(row);
+		for (ll i = 0; i < row; i++)
+		{
+			X[i].resize(col);
+		}
+		ll x;
+		for(ll i=0;i<row;i++){
+			for(ll j=0;j<col;j++){
+				std::cin>>x;
+				X[i][j]=RationalNumber(x,1);
+			}
+		}
+	}
+	*/
+
+	void output_matrix()
+	{
+		for (ll i = 0; i < row; i++)
+		{
+			for (ll j = 0; j < col; j++)
+			{
+				X[i][j].output();
+				std::cout << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+
+	void output_matrix_num()
+	{
+		for (ll i = 0; i < row; i++)
+		{
+			for (ll j = 0; j < col; j++)
+			{
+				X[i][j].output_num();
+				std::cout << " ";
+			}
+			std::cout << std::endl;
+		}
+	}
+	void row_swap(ll i, ll j)
+	{
+		if (i != j)
+		{
+			for (ll k = 0; k < col; k++)
+			{
+				std::swap(X[i][k], X[j][k]);
+			}
+		}
+	}
+
+	void column_swap(ll i, ll j)
+	{
+		if (i != j)
+		{
+			for (ll k = 0; k < row; k++)
+			{
+				std::swap(X[k][i], X[k][j]);
+			}
+		}
+	}
+
+	void row_multiply(ll i, Field2 c)
+	{
+		for (ll k = 0; k < col; k++)
+		{
+			X[i][k] *= c;
+		}
+	}
+
+	void column_multiply(ll j, Field2 c)
+	{
+		for (ll k = 0; k < row; k++)
+		{
+			X[k][j] *= c;
+		}
+	}
+
+	void row_plus(ll i, ll j, Field2 c)
+	{
+		for (ll k = 0; k < col; k++)
+		{
+			X[i][k] += c * X[j][k];
+		}
+	}
+
+	void column_plus(ll i, ll j, Field2 c)
+	{
+		for (ll k = 0; k < row; k++)
+		{
+			X[k][i] += c * X[k][j];
+		}
+	}
+
+	Matrix2 operator*(const Matrix2 &A)
+	{
+		Matrix2 B = Matrix2(row, A.col);
+
+		for(ll i=0;i<B.row;i++){
+			for(ll j=0;j<B.col;j++){
+				B.X[i][j].setZero();
+			}
+		}
+		for (ll i = 0; i < row; i++)
+		{
+			for (ll j = 0; j < A.col; j++)
+			{
+				for (ll k = 0; k < col; k++)
+				{
+					B.X[i][j] += X[i][k] * A.X[k][j];
+				}
+			}
+		}
+		return B;
+	}
+	Matrix2 operator-(const Matrix2& A)
+	{
+		Matrix2 B=Matrix2(row,col);
+		for(ll i=0;i<row;i++){
+			for(ll j=0;j<col;j++){
+				B.X[i][j]=X[i][j]-A.X[i][j];
+			}
+		}
+		return B;
+	}
+
+	void matrix_setZero(){
+		for(ll i=0;i<row;i++){
+			for(ll j=0;j<col;j++){
+				X[i][j].setZero();
+			}
+		}
+	}
+
+	Matrix2 sub_matrix(std::vector<ll> A, std::vector<ll> B)
+	{
+		Matrix2 S(A.size(), B.size());
+		for (ll i = 0; i < A.size(); i++)
+		{
+			for (ll j = 0; j < B.size(); j++)
+			{
+				S.X[i][j] = X[A[i]][B[j]];
+				//std::cout << i << " " << j << " " << A[i] << " " << B[j] << std::endl;
+				//S.X[i][j].output();
+				//std::cout << " ";
+				//X[A[i]][B[j]].output();
+				//std::cout << std::endl;
+			}
+		}
+		return S;
+	}
+
+	/*
+	num_type maximum_number()
+    {
+        num_type gamma=0;
+        for(ll i=0;i<row;i++){
+            for(ll j=0;j<col;j++){
+				//std::cout<<i<<" "<<j<<std::endl;
+                gamma=std::max(gamma,X[i][j].max_num());
+				//std::cout<<i<<" "<<j<<std::endl;
+            }
+        }
+        return gamma;
+    }
+	*/
+
+	
+};
+
+Matrix2 matrix2_inverse(const Matrix2& A)
+{
+	Matrix2 B=A;
+	ll c = A.col;
+	ll r = A.row;
+
+	if (r != c)
+	{
+		std::cout << "この行列のサイズは" << A.row << "*" << A.col << "なので逆行列を求められません" << std::endl;
+		
+		//exit(EXIT_FAILURE);
+	}
+
+	ll n = c;
+
+	Matrix2 A_inverse(n, n);
+
+		//A_inverse.output_matrix();
+
+	for (ll j = 0; j < n; j++)
+	{
+		Field2 mx;
+		mx.setZero();
+		ll pivot = j;
+		for (ll i = j; i < n; i++)
+		{
+			if (mx < B.X[i][j] || mx < -B.X[i][j])
+			{
+				mx = std::max(B.X[i][j], -B.X[i][j]);
+				pivot = i;
+			}
+		}
+		if (mx.isZero())
+		{
+			std::cout<<"A is not nonsingular"<<std::endl;
+			//A_inverse.matrix_setZero();
+			//return A_inverse;
+			exit(EXIT_FAILURE);
+		}
+
+		B.row_swap(j, pivot);
+		A_inverse.row_swap(j, pivot);
+
+		/* 			std::cout<<"aaa"<<std::endl;
+			output_matrix();
+			std::cout<<"aaa"<<std::endl;
+			A_inverse.output_matrix();
+			std::cout<<std::endl; */
+
+		Field2 a = B.X[j][j].inverse();
+
+		//multiply by a so that A[j][j]=1
+		B.row_multiply(j, a);
+		A_inverse.row_multiply(j, a);
+
+		for (ll i = 0; i < n; i++)
+		{
+			if (i != j)
+			{
+				Field2 b = B.X[i][j];
+				B.row_plus(i, j, -b);
+				A_inverse.row_plus(i, j, -b);
+				/*				for(ll k=0;k<n;k++){
+					A.row_plus(k,j,-b);
+					A.X[i][k]-=A.X[j][k]*b;
+					A_inverse.X[i][k]-=A_inverse.X[j][k]*b;
+				}*/
+			}
+		}
+	}
+	return A_inverse;
+}
+
+
+/* Matrix2 toMat2(Matrix &A)
+	{
+
+		Matrix2 B(A.row,A.col);
+		Matrix A_int=toINT(A);
+
+		for(ll i=0;i<A.row;i++){
+			for(ll j=0;j<A.col;j++){
+				B.X[i][j].num=A_int.X[i][j].numerator();
+			}
+		}
+		return B;
+	} */
+
+
+
+/* Matrix GaussJordan(Matrix &A)
+{
+	ll c = A.col;
+	ll r = A.row;
+
+	if (r != c)
+		exit(EXIT_FAILURE);
+
+	ll n = c;
+
+	Matrix A_inverse(n, n);
+	//A_inverse.output_matrix();
+
+	for (ll j = 0; j < n; j++)
+	{
+		Field mx;
+		mx.setZero();
+		ll pivot = j;
+		for (ll i = j; i < n; i++)
+		{
+			if (mx < A.X[i][j] || mx < -A.X[i][j])
+			{
+				mx = std::max(A.X[i][j], -A.X[i][j]);
+				pivot = i;
+			}
+		}
+		if (mx.isZero())
+			exit(EXIT_FAILURE);
+		A.row_swap(j, pivot);
+		A_inverse.row_swap(j, pivot);
+
+		Field a = A.X[j][j].inverse();
+
+		//multiply by a so that A[j][j]=1
+		A.row_multiply(j, a);
+		A_inverse.row_multiply(j, a);
+
+		for (ll i = 0; i < n; i++)
+		{
+			if (i != j)
+			{
+				Field b = A.X[i][j];
+				A.row_plus(i, j, -b);
+				A_inverse.row_plus(i, j, -b);
+				/*				for(ll k=0;k<n;k++){
+					A.row_plus(k,j,-b);
+					A.X[i][k]-=A.X[j][k]*b;
+					A_inverse.X[i][k]-=A_inverse.X[j][k]*b;
+				}*/
+/*
+			}
+		}
+	}
+	return A_inverse;
+}
+ */
